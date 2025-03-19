0610c552d4542e1aea41854c91b32124a5737cf7
s9096309 <k-hoff-mann@web.de>
2025-03-19 14:05:58 +0100

    Delete generate_hash.py

--- generate_hash.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-password = "your_test_password"  # Replace with your test password
-
-salt = bcrypt.gensalt()
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
-print(hashed_password.decode('utf-8'))
