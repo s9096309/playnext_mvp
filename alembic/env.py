1f349474028a7fa0715ad03892dfcb47c98318d7
s9096309 <k-hoff-mann@web.de>
2025-08-17 12:00:49 +0200

    Refactor Frontend to use new API BASE URL

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from database import Base
+from database.models import Base
 import os # Make sure os is imported
 import sys
 
