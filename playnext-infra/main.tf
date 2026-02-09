ece4715052b78021237de447733fb0351d62f8f4
s9096309 <k-hoff-mann@web.de>
2026-02-09 11:49:15 +0100

    chore(infra): Add Terraform configuration for AWS deployment

---
+++ playnext-infra/main.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "playnext-terraform-web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["2.208.149.34/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "playnext-terraform-db-sg"
+  description = "Allow Access from Web Server"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+}
+
+resource "aws_db_instance" "default" {
+  identifier           = "playnext-db-terraform"
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "17.4"
+  instance_class       = "db.t3.micro"
+  db_name              = "playnext_db"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.postgres17"
+  skip_final_snapshot  = true
+  publicly_accessible  = false
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-02003f9f0fde924ea"
+  instance_type = "t2.micro"
+  key_name      = "playnext-eb"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf update -y
+              sudo dnf install -y docker
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              sudo usermod -aG docker ec2-user
+              EOF
+
+  tags = {
+    Name = "PlayNext-App-Terraform"
+  }
+}
+
+output "web_server_ip" {
+  value = aws_instance.web.public_ip
+  description = "The public IP of your new server"
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.default.endpoint
+  description = "The address of your database (for env.py)"
+}
